b576b55a81a44863a8204675e18165310ea3242c
Andres1599 <higueros71@gmail.com>
2021-08-30 23:44:04 -0600

    add contract name and select version of compile for smart contracts

---
+++ contracts/OrderDelayed.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract OrderDelayed {
+    
+    struct Order {
+        uint token;
+        uint amount;
+    }
+
+    constructor() {}
+
+}
+
