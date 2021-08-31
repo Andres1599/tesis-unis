01fa37ad1bbf42179c58bd6b12b0918cf42bfdb7
Andres1599 <higueros71@gmail.com>
2021-08-30 23:56:48 -0600

    add initial structurs to No delivery Order

--- contracts/NoDeliveryOrder.sol
+++ contracts/NoDeliveryOrder.sol
@@ -3,5 +3,17 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract NoDeliveryOrder {
 
+    // struct to define the current state of a order
+    struct StateOrder {
+        uint256 times;
+        string status;
+    }
+    
+    // struct to define a order to be processed
+    struct Order {
+        uint orderToken;
+        StateOrder state;
+    }
+    
     constructor() {}
 }
