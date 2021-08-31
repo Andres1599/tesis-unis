66a55d1ef1cf4991813c04ef85ac03eac7cb4b41
Andres1599 <higueros71@gmail.com>
2021-08-30 23:46:37 -0600

    add contracto on the migration file to be able to upload the smart contracts to blockchain network

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,10 @@
+
 const Migrations = artifacts.require("Migrations");
+const NoDeliveryOrder = artifacts.require("NoDeliveryOrder");
+const OrderDelayed = artifacts.require("OrderDelayed");
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(NoDeliveryOrder);
+  deployer.deploy(OrderDelayed);
 };
