c33b41ab61775d1bf4d8ac1e5469edcff2a9cefe
Andres1599 <higueros71@gmail.com>
2021-09-04 19:50:24 -0600

    create a migration file to allow truffle compile contracts

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,10 +1,6 @@
 
 const Migrations = artifacts.require("Migrations");
-const NoDeliveryOrder = artifacts.require("NoDeliveryOrder");
-const OrderDelayed = artifacts.require("OrderDelayed");
 
-module.exports = function (deployer) {
+module.exports = deployer => {
   deployer.deploy(Migrations);
-  deployer.deploy(NoDeliveryOrder);
-  deployer.deploy(OrderDelayed);
 };
