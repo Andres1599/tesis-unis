c41d3eebb561000162570d97f3d2451975981586
Andres1599 <higueros71@gmail.com>
2021-09-04 19:43:30 -0600

    create a simulation of oracle in

--- contracts/OrderDelayed.sol
+++ contracts/OrderDelayed.sol
@@ -3,6 +3,11 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract OrderDelayed {
 
+    address owner;
+    uint public numberAsteroids;
+
+    event __callBackNewData();
+
     // struct to define the current state of a order
     struct StateOrder {
         uint256 times;
@@ -15,7 +20,24 @@ contract OrderDelayed {
         StateOrder state;
     }
 
-    constructor() {}
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can do this");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    // function to emit the event on externarl application
+    function updateData() public onlyOwner {
+        emit __callBackNewData();
+    }
+
+    // function to set the informetion about the current state
+    function setData(uint _numberAsteroids) public onlyOwner {
+        numberAsteroids = _numberAsteroids;
+    }
 
 }
 
